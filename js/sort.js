aa87ab090d5cd328de62537c85ed4a89c91527c2
liutao <947809647@qq.com>
2020-03-22 21:45:00 +0800

    feat: 新增排序

---
+++ js/sort.js
@@ -0,0 +1,18 @@
+// 冒泡排序
+function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+// 测试
+let array = [3, 2, 8, 5, 6, 4];
+
+console.log(bubbleSort(array));
